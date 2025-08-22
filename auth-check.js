b9a139d1aaea7fca0585f13acc2e84f9ba3e6ede
nonsensicalraven <sahithireddy422@gmail.com>
2025-08-23 05:13:43 +0530

    exclusive access to artform page

--- auth-check.js
+++ auth-check.js
@@ -1,11 +1,17 @@
+// auth-check.js
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+// Initialize Firebase Auth
 const auth = getAuth();
 
+// Check if user is logged in
 onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    // User not logged in
+  if (user) {
+    // User is signed in
+    console.log("User is logged in:", user.email);
+  } else {
+    // No user is signed in, redirect to login or homepage
     alert("You must be logged in to access this page!");
-    window.location.href = "index.html"; // redirect to login/home page
+    window.location.href = "index.html"; // redirect to homepage or login page
   }
 });
