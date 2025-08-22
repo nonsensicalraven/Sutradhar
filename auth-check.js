cb450673cd29067f9d5d56cbbaa305f8dc5be6c1
nonsensicalraven <sahithireddy422@gmail.com>
2025-08-23 05:25:41 +0530

    firebase...

--- auth-check.js
+++ auth-check.js
@@ -1,17 +1,22 @@
-// auth-check.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-// Initialize Firebase Auth
-const auth = getAuth();
+const firebaseConfig = {
+    apiKey: "AIzaSyBv23cbRzW4y224z3YDPPMvGMkx0k-Mxu8",
+    authDomain: "sutradhar-2c17d.firebaseapp.com",
+    projectId: "sutradhar-2c17d",
+    storageBucket: "sutradhar-2c17d.firebasestorage.app",
+    messagingSenderId: "527545107973",
+    appId: "1:527545107973:web:a7f1548107c826429bbe6c",
+    measurementId: "G-86KK9NQDDT"
+};
 
-// Check if user is logged in
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in
-    console.log("User is logged in:", user.email);
-  } else {
-    // No user is signed in, redirect to login or homepage
-    alert("You must be logged in to access this page!");
-    window.location.href = "index.html"; // redirect to homepage or login page
-  }
+    if (!user) {
+        alert("You must log in first!");
+        window.location.href = "index.html";
+    }
 });
